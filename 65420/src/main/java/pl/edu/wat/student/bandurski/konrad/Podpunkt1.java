46a35ec9a6ff8412b515da41e5b0bc40e627f89b
Konrad Bandurski <konrad.bandurski@student.wat.edu.pl>
2019-11-30 15:45:38 +0100

    Restore lab1

---
+++ 65420/src/main/java/pl/edu/wat/student/bandurski/konrad/Podpunkt1.java
@@ -0,0 +1,50 @@
+package pl.edu.wat.student.bandurski.konrad;
+
+import com.joptimizer.exception.JOptimizerException;
+import com.joptimizer.functions.ConvexMultivariateRealFunction;
+import com.joptimizer.functions.LinearMultivariateRealFunction;
+import com.joptimizer.optimizers.JOptimizer;
+import com.joptimizer.optimizers.OptimizationRequest;
+
+
+public class Podpunkt1
+{
+    public static void main(String[] args) throws JOptimizerException {
+        // Initialize objective function
+        LinearMultivariateRealFunction objectiveFunction =
+                new LinearMultivariateRealFunction(new double[] {-50.0, -10.0}, 0); //50x1 + 10x2 -> max
+
+        // Initialize inequalities constraints
+        ConvexMultivariateRealFunction[] constraints = new ConvexMultivariateRealFunction[4];
+        // x >= 0
+        constraints[0] = new LinearMultivariateRealFunction(new double[] {-1.0, 0.0}, 0.0);
+        // y >= 0
+        constraints[1] = new LinearMultivariateRealFunction(new double[] {0.0, -1.0}, 0.0);
+        // 12x1 + 4x2 <= 480
+        constraints[2] = new LinearMultivariateRealFunction(new double[] {12.0, 4.0}, -480.0);
+        // 8x1 + 8x2 <= 640
+        constraints[3] = new LinearMultivariateRealFunction(new double[] {8.0, 8.0}, -640.0);//
+
+        // Initialize optimization request
+        OptimizationRequest request = new OptimizationRequest();
+        request.setF0(objectiveFunction);
+        request.setFi(constraints);
+
+        // Set tolerance
+        request.setToleranceFeas(1.E-12);
+        request.setTolerance(1.E-12);
+
+        // Initialize JOptimizer and get optimization result
+        JOptimizer optimizer = new JOptimizer();
+        optimizer.setOptimizationRequest(request);
+        optimizer.optimize();
+
+        double[] solution = optimizer.getOptimizationResponse().getSolution();
+
+        // Print result
+        System.out.println("Length: " + solution.length);
+        for (int i = 0; i < solution.length / 2; i++) {
+            System.out.println("X" + (i + 1) + ": " + Math.round(solution[i]) + "\tY" + (i + 1) + ": " + Math.round(solution[i + 1]));
+        }
+    }
+}
