aa59828dad6027d6e1871fa1387838f97f33e953
Rafał Huk <r.huk@outlook.com>
2019-12-02 14:33:28 +0100

    Dodanie base64

--- $65431/Lab2_65431/scrape2.js
+++ $65431/Lab2_65431/scrape2.js
@@ -15,12 +15,12 @@ const location = args[5];
 var announcement;
 var $;
 var offer = new Object();
-var i = 0;
-var a;
+var offers = []
+
 
 
 scrape(1);
-function scrape(actualPage){
+ function scrape(actualPage){
 
 request('https://www.otomoto.pl/osobowe/'+brand+'/'+model+'/'+location+'/'+
 '?search[filter_float_price%3Afrom]='+priceFrom+'&search[filter_float_price%3Ato]='+priceTo+
@@ -30,86 +30,90 @@ request('https://www.otomoto.pl/osobowe/'+brand+'/'+model+'/'+location+'/'+
             $ = cheerio.load(html);            
             announcement = $('.adListingItem').toArray();
             var pg = parseInt($('.page').text(),10);
-            var offers = []
+            
+            var numberOfAnnoucements = $('.selected .counter').text()
+            numberOfAnnoucements = numberOfAnnoucements.replace("(","").replace(")","")
 
-          
 
-console.log('https://www.otomoto.pl/osobowe/'+brand+'/'+model+'/'+location+'/'+
-'?search[filter_float_price%3Afrom]='+priceFrom+'&search[filter_float_price%3Ato]='+priceTo+
-'&search[filter_enum_fuel_type][0]='+fuel+'&page='+actualPage)
 
 
          announcement.forEach( el=> {
-            offer.car = $('.offer-title__link', el)
-                .text()
-                .replace(/(\r\n|\n|\r|  +)/gm,"");
-            
-            offer.subtitle = $('.offer-item__subtitle', el)
-                .text()
-                .replace(/(\r\n|\n|\r|  +)/gm,"");
-            
-            offer.title = $('.offer-item__title', el)
-                .text()
-                .replace(/(\r\n|\n|\r|  +)/gm,"");
 
-            offer.year = $('.ds-params-block [data-code=year]', el)
-                .text()
-                .replace(/(\r\n|\n|\r|  +)/gm,"");
-
-            offer.mileage = $('.ds-params-block [data-code=mileage]', el)
-                .text()
-                .replace(/(\r\n|\n|\r|  +)/gm,"");
-
-            offer.engine_capacity = $('.ds-params-block [data-code=engine_capacity]', el)
-                .text()
-                .replace(/(\r\n|\n|\r|  +)/gm,"");
-
-            offer.fuel_type = $('.ds-params-block [data-code=fuel_type]', el)
-                .text()
-                .replace(/(\r\n|\n|\r|  +)/gm,"");
-
-            offer.price=$('.offer-price', el)
-                .text()
-                .replace(/(\r\n|\n|\r|  +)/gm,"");
 
             offer.imgURL=$('img', el)
                  .attr('data-src');
 
-                image2base64(offer.imgURL)
-                // .then((response)=> {
-                //     console.log(response) 
-                // }
-                //     )
-                // .catch(
-                //     (error) => {
-                //         console.log(error); 
-                //     }
-                // )                
+               offer.base64=image2base64(offer.imgURL)
+                .then((response)=> {
+                offer.base64= response
+                    offer.car = $('.offer-title__link', el)
+                    .text()
+                    .replace(/(\r\n|\n|\r|  +)/gm,"");
+                
+                offer.subtitle = $('.offer-item__subtitle', el)
+                    .text()
+                    .replace(/(\r\n|\n|\r|  +)/gm,"");
+                
+                offer.title = $('.offer-item__title', el)
+                    .text()
+                    .replace(/(\r\n|\n|\r|  +)/gm,"");
+    
+                offer.year = $('.ds-params-block [data-code=year]', el)
+                    .text()
+                    .replace(/(\r\n|\n|\r|  +)/gm,"");
+    
+                offer.mileage = $('.ds-params-block [data-code=mileage]', el)
+                    .text()
+                    .replace(/(\r\n|\n|\r|  +)/gm,"");
+    
+                offer.engine_capacity = $('.ds-params-block [data-code=engine_capacity]', el)
+                    .text()
+                    .replace(/(\r\n|\n|\r|  +)/gm,"");
+    
+                offer.fuel_type = $('.ds-params-block [data-code=fuel_type]', el)
+                    .text()
+                    .replace(/(\r\n|\n|\r|  +)/gm,"");
+    
+                offer.price=$('.offer-price', el)
+                    .text()
+                    .replace(/(\r\n|\n|\r|  +)/gm,"");
 
-                 offers.push(new Object({
-                    car: offer.car,
-                    title: offer.subtitle,
-                    year: offer.year,
-                    mileage: offer.mileage,
-                    engine_capacity: offer.engine_capacity,
-                    fuel_type: offer.fuel_type,
-                    price: offer.price,
-                    imgURL: offer.imgURL,
-                    base64: offer.base64
-                }));
-
-              i=i+1
+                    offers.push(new Object({
+                        car: offer.car,
+                        title: offer.subtitle,
+                        year: offer.year,
+                        mileage: offer.mileage,
+                        engine_capacity: offer.engine_capacity,
+                        fuel_type: offer.fuel_type,
+                        price: offer.price,
+                        imgURL: offer.imgURL,
+                        base64: offer.base64
+                    }));
+                    console.log("Rozmiar tablicy: "+offers.length)
+                    
+                   if(offers.length+1==parseInt(numberOfAnnoucements))
+                        fs.writeFileSync('otomoto.json', JSON.stringify(offers));  
+                }
+ 
+                    )
+                .catch(
+                    (error) => {
+                        console.log(error); 
+                    }
+                )                
             });
-            console.log("a: "+a);
             if(actualPage<pg)
             {
                 actualPage++;
                 scrape(actualPage);
             }
-         //  console.log(offers);
-            fs.writeFileSync('otomoto.json', JSON.stringify(offers));
+            
+
             
 
         }});
     }
+
+
+    
 
