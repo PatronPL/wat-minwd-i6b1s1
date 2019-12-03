d0057ea9b28d736fb9e458e5f67006efd8f4f8b4
Damian <damianzylka@outlook.com>
2019-12-03 09:18:34 +0100

    Fix problem with client

---
+++ lab3/65471_65431/src/app/http.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  url="https://localhost:44373/data/";
+  constructor(private http: HttpClient) { }
+
+  getAllTrams() {
+    return this.http.get(this.url);
+  }
+  getByLine(number) {
+    return this.http.get(this.url+"line/"+number);
+  }
+
+}
