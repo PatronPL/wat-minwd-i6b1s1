07878eacf5b2c5c68f08d795e9dc658a06f4adfe
wstrankowski <45851836+wstrankowski@users.noreply.github.com>
2019-12-04 13:43:01 +0100

    lab2

---
+++ lab2/65452/README.md
@@ -0,0 +1,20 @@
+Wojciech Strankowski
+
+Zadanie: "Słownik" - scrape wikisłownika
+
+## Wymagania
+
+.NET Framework
+Przeglądarka Google Chrome
+
+## Uruchamianie
+
+Należy otworzyć projekt w VS, wybrać zarządanie pakietami nuget i nacisąć restore/przywróć, a następnie uruchomić.
+
+## Obsługa
+
+Należy wpisać w lewym górnym rogu angielskie słowo, które chcemy przetłumaczyć i nacisnąć "tłumacz".
+Należy poczekać, aż przeglądrka się otworzy i zamknie automatycznie.
+Znalezione tłumaczenie oraz zdjęcie pojawią się w odpowiednich miejscach, bądź inforamcja o braku tłumaczeń w przypadku błędnego słowa.
+
+
---
+++ lab2/65452/WikiScrape/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WikiScrape
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.translations = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.wordInput = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // translations
+            // 
+            this.translations.Location = new System.Drawing.Point(359, 31);
+            this.translations.Multiline = true;
+            this.translations.Name = "translations";
+            this.translations.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.translations.Size = new System.Drawing.Size(395, 382);
+            this.translations.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tłumacz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // wordInput
+            // 
+            this.wordInput.Location = new System.Drawing.Point(12, 31);
+            this.wordInput.Name = "wordInput";
+            this.wordInput.Size = new System.Drawing.Size(198, 22);
+            this.wordInput.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 96);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(316, 317);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Obrazek";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(356, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tłumaczenia";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.wordInput);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.translations);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox translations;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox wordInput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
