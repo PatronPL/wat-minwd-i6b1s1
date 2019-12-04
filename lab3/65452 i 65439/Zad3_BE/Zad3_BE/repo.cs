62178221e38ec3149e0cee775364eb1f57e6a28e
ThatKidMike <44540485+ThatKidMike@users.noreply.github.com>
2019-12-04 10:49:09 +0100

    65439 oraz 65452 zadanie 3.

---
+++ lab3/65452 i 65439/Zad3_BE/Zad3_BE/repo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Zad3_BE
+{
+    public class repo
+    {
+        private readonly IHttpClientFactory clientFactory;
+
+        public repo(IHttpClientFactory clientFactory)
+        {
+            this.clientFactory = clientFactory;
+        }
+
+
+    }
+}
